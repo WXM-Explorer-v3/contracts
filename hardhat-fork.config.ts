14bda1d5338213032050b7c6f75c183825150522
teo <tmavroeidakos@gmail.com>
2024-04-24 11:02:25 +0300

    fix: replace mumbai rpc with arbitrum sepolia rpc.

--- hardhat-fork.config.ts
+++ hardhat-fork.config.ts
@@ -9,8 +9,8 @@ export default {
       allowUnlimitedContractSize: false,
       blockGasLimit: 20000000,
       forking: {
-        url: `https://polygon-mumbai.g.alchemy.com/v2/${envConfig.ALCHEMY_API_KEY}`,
-        blockNumber: 32905905
+        url: `https://arb-sepolia.g.alchemy.com/v2/${envConfig.ALCHEMY_API_KEY}`,
+        blockNumber: 36949713
       }
     }
   }
